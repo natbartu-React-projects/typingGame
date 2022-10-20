7861bc948010407bdefdff34db5dcc125d80df68
natacha <nat_bartu@hotmail.com>
2022-10-20 12:26:00 +1000

    add animation state value

--- src/App.js
+++ src/App.js
@@ -5,6 +5,13 @@ import "./App.css";
 const App = () => {
   const [word, setWord] = useState(Words);
   const [newWord, setNweWord] = useState(word[0]);
+  const [disable, setDisable] = useState(true);
+  const [currentResults, setCurrentResults] = useState([]);
+  const [incorrectResults, setIncorrectResults] = useState([]);
+  const [count, setCount] = useState(0);
+  const [time, setTime] = useState(30);
+  const [inputValue, setInputValue] = useState("");
+  const [animation, setAnimation] = useState(null);
 
   return <div className="App"></div>;
 };
