2fe1cd2975109a6b6562455f1736ead2f186ca7b
natacha <nat_bartu@hotmail.com>
2022-10-20 11:39:23 +1000

    create a components folder, and button component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Starter</h1>
-      </header>
-    </div>
-  );
+  return <h1>Starter</h1>;
 }
 
 export default App;
